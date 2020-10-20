aacf32943b76e116ac2a58d6eee4fc2c4e1f1ce1
Adrian Fu <adrian101fu@gmail.com>
2020-10-20 17:14:19 +1100

    Tab to space

--- ExploratoryDataAnalysis.py
+++ ExploratoryDataAnalysis.py
@@ -2,6 +2,6 @@ class ExploratoryDataAnalysis:
     def __init__(self, train_val_X, train_val_y):
         self.train_val_X = train_val_X
         self.train_val_y = train_val_y
-        
-	def DuplicatedRows(self):
-		return train_val_X.duplicated()
+
+    def DuplicatedRows(self):
+        return train_val_X.duplicated()
