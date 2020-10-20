32239c2c81bcb6d42d8942710a17976b7b049283
Adrian Fu <adrian101fu@gmail.com>
2020-10-20 17:01:27 +1100

    ...

--- ExploratoryDataAnalysis.py
+++ ExploratoryDataAnalysis.py
@@ -1,3 +1,7 @@
 class ExploratoryDataAnalysis:
+    def __init__(self, train_val_X, train_val_y):
+        self.train_val_X = train_val_X
+        self.train_val_y = train_val_y
+        
 	def DuplicatedRows(self):
-		return train_X.duplicated()
+		return train_val_X.duplicated()
