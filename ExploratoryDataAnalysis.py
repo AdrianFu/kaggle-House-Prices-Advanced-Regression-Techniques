1e5e6cd6d0b7d4b9042590b44c312940170a2a0f
Adrian Fu <adrian101fu@gmail.com>
2020-10-21 16:00:08 +1100

    Cluster corr matrix

--- ExploratoryDataAnalysis.py
+++ ExploratoryDataAnalysis.py
@@ -1,3 +1,5 @@
+import scipy.cluster.hierarchy as sch
+
 class ExploratoryDataAnalysis:
     #def __init__(self, train_val_X, train_val_y):
     #    self.train_val_X = train_val_X
@@ -5,5 +7,34 @@ class ExploratoryDataAnalysis:
 
     def DuplicatedRows(self):
         return self.train_val_X.duplicated()
+
+    def cluster_corr(corr_array, inplace=False):
+        """
+        Rearranges the correlation matrix, corr_array, so that groups of highly
+        correlated variables are next to eachother
+
+        Parameters
+        ----------
+        corr_array : pandas.DataFrame or numpy.ndarray
+            a NxN correlation matrix 
+
+        Returns
+        -------
+        pandas.DataFrame or numpy.ndarray
+            a NxN correlation matrix with the columns and rows rearranged
+        """
+        pairwise_distances = sch.distance.pdist(corr_array)
+        linkage = sch.linkage(pairwise_distances, method='complete')
+        cluster_distance_threshold = pairwise_distances.max()/2
+        idx_to_cluster_array = sch.fcluster(linkage, cluster_distance_threshold,
+            criterion='distance')
+        idx = np.argsort(idx_to_cluster_array)
+
+        if not inplace:
+            corr_array = corr_array.copy()
+
+        if isinstance(corr_array, pd.DataFrame):
+            return corr_array.iloc[idx, :].T.iloc[idx, :]
+        return corr_array[idx, :][:, idx]
 
 EDA = ExploratoryDataAnalysis()
